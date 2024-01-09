54f4bcd2b0c0bdf0caf11c800349ce2d8b80cfe8
Freilyn Bernabe <freilynbpwork@gmail.com>
2024-01-09 08:55:59 -0400

    add vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel({
+    analytics: true,
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
